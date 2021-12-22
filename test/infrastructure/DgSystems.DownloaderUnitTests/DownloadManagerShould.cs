4e1449320777131a2ab186d983ed2e36abad59a7
Domício Medeiros <domicioam@gmail.com>
2021-12-22 16:51:10 +0000

    validate not copy when download fails

--- test/infrastructure/DgSystems.DownloaderUnitTests/DownloadManagerShould.cs
+++ test/infrastructure/DgSystems.DownloaderUnitTests/DownloadManagerShould.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using System;
 using System.IO.Abstractions.TestingHelpers;
+using System.Net;
 using System.Net.Http;
 using Xunit;
 
@@ -9,22 +10,43 @@ namespace DgSystems.DownloaderUnitTests
 {
     public class DownloadManagerShould
     {
+        private const string eclipseUrl = "http://localhost/eclipse.exe";
+        private const string downloadFolder = "C:\\Downloads";
+
         [Fact]
         public async void Download()
         {
             // Arrange
-            var httpResponseMessage = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK };
+            var httpResponseMessage = new HttpResponseMessage() { StatusCode = HttpStatusCode.OK };
             var httpMessageHandler = new MockHttpMessageHandler(httpResponseMessage);
             var httpClient = new HttpClient(httpMessageHandler);
             var fileSystem = new MockFileSystem();
-            fileSystem.AddDirectory("C:\\Downloads");
+            fileSystem.AddDirectory(downloadFolder);
             var downloader = new DownloadManager(httpClient, fileSystem);
 
             // Act
-            await downloader.DownloadFile(new Uri("http://localhost/eclipse.exe"), "C:\\Downloads");
+            await downloader.DownloadFile(new Uri(eclipseUrl), downloadFolder);
             
             // Assert
             downloader.IsSuccess().Should().BeTrue();
+        }
+
+        [Fact]
+        public async void NotCopyWhenDownloadFails()
+        {
+            // Arrange
+            var httpResponseMessage = new HttpResponseMessage() { StatusCode = HttpStatusCode.NotFound };
+            var httpMessageHandler = new MockHttpMessageHandler(httpResponseMessage);
+            var httpClient = new HttpClient(httpMessageHandler);
+            var fileSystem = new MockFileSystem();
+            fileSystem.AddDirectory(downloadFolder);
+            var downloader = new DownloadManager(httpClient, fileSystem);
+
+            // Act
+            await Assert.ThrowsAsync<HttpRequestException>(() => downloader.DownloadFile(new Uri(eclipseUrl), downloadFolder));
+
+            // Assert
+            downloader.IsSuccess().Should().BeFalse();
         }
     }
 }
