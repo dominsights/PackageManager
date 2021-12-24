17a3e6da0d898c1f44d42155b38761971f055c9b
Domício Medeiros <domicioam@gmail.com>
2021-12-24 11:58:40 +0000

    create unit test project for powershell

---
+++ test/infrastructure/DgSystems.PowerShellUnitTests/PowerShellUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace DgSystems.PowerShellUnitTests
+{
+    public class PowerShellUnitTests
+    {
+        [Fact]
+        public void MyTestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
