444e5703484c9a5d8a2deacd1dd86f97661d609a
Domício Medeiros <domicioam@gmail.com>
2021-12-26 07:12:14 +0000

    test projects for adapters

---
+++ test/core/PackageManager/UseCases/InstallPackage/InstallPackageInteractorShould.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DgSystems.PackageManagerUnitTests.UseCases.InstallPackage
+{
+    internal class InstallPackageInteractorShould
+    {
+    }
+}
