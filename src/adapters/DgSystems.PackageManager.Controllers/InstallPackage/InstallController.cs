a8128736a22ab057c427bcd507be514cc46a8e62
Domício Medeiros <domicioam@gmail.com>
2021-12-26 11:55:07 +0000

    ...

--- src/adapters/DgSystems.PackageManager.Controllers/InstallPackage/InstallController.cs
+++ src/adapters/DgSystems.PackageManager.Controllers/InstallPackage/InstallController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DgSystems.PackageManager.Controllers
+namespace DgSystems.PackageManager.Controllers.InstallPackage
 {
     public class InstallController
     {
--- src/adapters/DgSystems.PackageManager.Presenters/InstallPackage/InstallPackagePresenter.cs
+++ src/adapters/DgSystems.PackageManager.Presenters/InstallPackage/InstallPackagePresenter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DgSystems.PackageManager.Presenters
+namespace DgSystems.PackageManager.Presenters.InstallPackage
 {
     internal class InstallPackagePresenter : OutputBoundary
     {
