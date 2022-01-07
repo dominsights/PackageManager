375ff924031908fb975ee1bb099a7aeb22f265d0
Domício Medeiros <domicioam@gmail.com>
2022-01-07 13:46:19 +0000

    specflow setup

--- test/infrastructure/DgSystems.ScoopUnitTests/Buckets/Commands/SyncRepositoryShould.cs
+++ test/infrastructure/DgSystems.ScoopUnitTests/Buckets/Commands/SyncRepositoryShould.cs
@@ -4,8 +4,6 @@ using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace DgSystems.ScoopUnitTests.Commands
